4fbd35732b9f0eebe386614623f3f40ec9d377b4
JonasSatkauskas <satkusmedia@gmail.com>
2021-09-21 12:01:44 +0300

    Fixed to get one cat

--- list.py
+++ list.py
@@ -1,12 +1,24 @@
 import json
 import boto3
+from boto3.dynamodb.conditions import Key
 
 
-def lambda_handler(event, context):  
-    dynamodb = boto3.resource('dynamodb')  
-    catsTable = dynamodb.Table('CATS')  
-    response = catsTable.scan()  
-    return {    
-        'statusCode': 200,    
-        'body': response['Items']  
+def lambda_handler(event, context):
+    dynamodb_client = boto3.client('dynamodb', region_name="us-east-2")
+    try:
+        response = dynamodb_client.query(
+        TableName='CATS',
+        KeyConditionExpression='cat_id = :cat_id',
+        ExpressionAttributeValues={
+            ':cat_id': {'S': '1'}
+            }
+        )
+        return {    
+            'statusCode': 200,    
+            'body': response['Items']  
     }
+    except:
+        return {    
+            'statusCode': 400,    
+            'body': json.dumps(f"We could not retrieve these cats.")  
+        }
