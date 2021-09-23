bc11f7a887c6ae57ceb1fb48bc01375098815e2b
JonasSatkauskas <satkusmedia@gmail.com>
2021-09-23 19:41:51 +0300

    Finished function

--- list.py
+++ list.py
@@ -5,27 +5,30 @@ from boto3.dynamodb.conditions import Key
 
 def lambda_handler(event, context):
 
-    #grab the category-id from the url, suppose url is in the form /list?id=1, where id is category id
     
     id = 0
     if 'cat_id' in event:
         id = event['cat_id']
         cat_list = id.split(',') 
         
-
     dynamodb_client = boto3.client('dynamodb', region_name="us-east-2")
     try:
-        response = dynamodb_client.query(
-        TableName='CATS',
-        KeyConditionExpression='cat_id = :a',
-        ExpressionAttributeValues={
-                ':a': {'S': id}
-                }
-        )
+        resp = []
+        for i in cat_list:
+            response = dynamodb_client.query(
+            TableName='CATS',
+            KeyConditionExpression='cat_id = :a',
+            ExpressionAttributeValues={
+                    ':a': {'S': i}
+                    }
+            )
+            resp.append(response['Items'])
+        
         return {    
             'statusCode': 200,    
-            'body': response['Items']  
+            'body': resp
         }
+        
     except:
         return {    
             'statusCode': 400,    
